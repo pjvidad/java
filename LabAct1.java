2f39ea02ac23cee9e574896aa98fde83a1b76d3a
Princess Joy Vidad <145944544+pjvidad@users.noreply.github.com>
2024-01-12 17:02:43 +0800

    Update LabAct1.java

--- LabAct1.java
+++ LabAct1.java
@@ -5,35 +5,35 @@ class LabAct1 {
 
     System.out.println("An Algorithm to Prepare to Sleep:");
     System.out.println("Start");
-    System.out.println("Take a hot bath");
-    System.out.println("Change into pajamas");
-    System.out.println("Brush teeth");
-    System.out.println("Floss teeth");
-    System.out.println("Wear retainers");
-    System.out.println("Apply cleanser and toner on my face");
-    System.out.println("Go to bed");
-    System.out.println("Read a book");
+    System.out.println("1. Take a hot bath");
+    System.out.println("2. Change into pajamas");
+    System.out.println("3. Brush teeth");
+    System.out.println("4. Floss teeth");
+    System.out.println("5. Wear retainers");
+    System.out.println("6. Apply cleanser and toner on my face");
+    System.out.println("7. Go to bed");
+    System.out.println("8. Read a book");
 
     Scanner input = new Scanner(System.in);
-    System.out.print("Is today a weekday or a weekend? ");
+    System.out.print("Q: Is today a weekday or a weekend? ");
     String answer = input.nextLine();
     input.close();
 
     if(answer.toLowerCase().equals("weekday")) {
-    System.out.println("Turn off the lights");
-    System.out.println("Pray");
-    System.out.println("Go to sleep");
+    System.out.println("9. Turn off the lights");
+    System.out.println("10. Pray");
+    System.out.println("11. Go to sleep");
     System.out.println("End");
     }
     else if(answer.toLowerCase().equals("weekend")) {
-    System.out.println("Play Stardew Valley until sleepy");
-    System.out.println("Turn off the lights");
-    System.out.println("Pray");
-    System.out.println("Go to sleep");
+    System.out.println("9. Play Stardew Valley until sleepy");
+    System.out.println("10. Turn off the lights");
+    System.out.println("11. Pray");
+    System.out.println("12. Go to sleep");
     System.out.println("End");
     }
     else {
-    System.out.println("'Weekday' or 'Weekend' option only! Run the code again.no");
+    System.out.println("'Weekday' or 'Weekend' option only! Run the code again.");
     }
     }
     
