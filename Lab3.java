d804fc95596b3a7095cad60ea116b9d89cf086a1
Princess Joy Vidad <145944544+pjvidad@users.noreply.github.com>
2024-02-05 20:40:22 +0800

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -16,32 +16,29 @@ public class Lab3 {
         System.out.print("Enter number of Dimensions: ");
         int dimension = input.nextInt();
 
-        input.close();
-        
         // upper bounds
-        int[] upperBounds = new int[dimension];
+        int[] upperBounds = new int[dimension], indices = new int[dimension];
         for (int i = 0; i < dimension; i++) {
             System.out.print("Enter upper bound " + (i + 1) + " : ");
             upperBounds[i] = input.nextInt();
         }
-
         // indices
-        int[] indices = new int[dimension];
         for (int j = 0; j < dimension; j++) {
             System.out.print("Enter array index " + (j + 1) + " : ");
             indices[j] = input.nextInt();
         }
 
+        input.close();
+
         // assign method to a variable
         long rowMajorAddress = calculateRMSAddress(base, weight, upperBounds, indices);
 
         //displays output
-        System.out.println("Row-major address: " + rowMajorAddress);
+        System.out.println("RMS: " + rowMajorAddress);
     }
 
     public static long calculateRMSAddress(int base, int weight, int[] upperBounds, int[] indices) {
-        int dimensions = upperBounds.length;
-        int product = 1;
+        int dimensions = upperBounds.length, product = 1;
 
         // calculate product of upper bounds
         for (int i = 1; i < dimensions; i++) {
@@ -56,7 +53,6 @@ public class Lab3 {
                 product /= upperBounds[i + 1];
             }
         }
-
         return rowMajorAddress;
     }
 }
